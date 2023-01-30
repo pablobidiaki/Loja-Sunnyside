64f87c04f94c742ce3a1005b0458bf5fb0d9f3c2
pablobidiaki <pablobidiaki@hotmail.com>
2023-01-30 15:32:40 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const barras = document.getElementById('bars')
 const bar1 = document.getElementById('bar1')
 const bar2 = document.getElementById('bar2')
 const bar3 = document.getElementById('bar3')
+const modal = document.getElementById('modal')
 let contagem = 0
 
 barras.addEventListener('click', () => {
@@ -10,9 +11,11 @@ barras.addEventListener('click', () => {
         bar1.style.transform= 'rotate(-45deg) translateY(4.8px) translateX(-4.2px)'
         bar2.style.transform = 'rotate(45deg)'
         bar3.style.opacity = '0'
+        modal.style.opacity = '1'
     } else {
         bar1.style.transform= 'rotate(0deg) translateY(0px) translateX(0px)'
         bar2.style.transform = 'rotate(0)'
         bar3.style.opacity = '1'
+        modal.style.opacity = '0'
     }
 })
