23668817fe4b8c5fb0a00d568e0069fad0a4d22b
pablobidiaki <pablobidiaki@hotmail.com>
2023-01-30 11:22:18 -0300

    Projeto em andamento

---
+++ script.js
@@ -0,0 +1,18 @@
+const barras = document.getElementById('bars')
+const bar1 = document.getElementById('bar1')
+const bar2 = document.getElementById('bar2')
+const bar3 = document.getElementById('bar3')
+let contagem = 0
+
+barras.addEventListener('click', () => {
+    contagem += 1
+    if(contagem % 2 === 1){
+        bar1.style.transform= 'rotate(-45deg) translateY(4.8px) translateX(-4.2px)'
+        bar2.style.transform = 'rotate(45deg)'
+        bar3.style.opacity = '0'
+    } else {
+        bar1.style.transform= 'rotate(0deg) translateY(0px) translateX(0px)'
+        bar2.style.transform = 'rotate(0)'
+        bar3.style.opacity = '1'
+    }
+})
